8da08674cedb8591264b54991cf69dca18530a9c
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-02-10 17:32:44 +0100

    started features

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { useGlobalContext } from '../context';
 
 const Featured = () => {
+  const { products, loading } = useGlobalContext();
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
   return (
     <Section>
       <Header>
         <h2>Featured Products</h2>
         <Underline></Underline>
       </Header>
-      <Products></Products>
+      <Products>
+        {products
+          .filter((product) => product.featured)
+          .map((filteredProduct) => (
+            <Grid>
+              <Img src={filteredProduct.image} alt={filteredProduct.name} />
+
+              <ProductInfo>
+                <p>{filteredProduct.name}</p>
+                <p>${filteredProduct.price / 100}</p>
+              </ProductInfo>
+            </Grid>
+          ))}
+      </Products>
     </Section>
   );
 };
@@ -22,8 +41,11 @@ const Section = styled.section`
 
 const Header = styled.header`
   text-align: center;
-  font-size: 2rem;
+  font-size: 1.5rem;
   color: #102a42;
+   @ media(max-width:1000px){
+    font-size: 2rem;
+  }
 `;
 
 const Underline = styled.div`
@@ -33,5 +55,26 @@ const Underline = styled.div`
   margin: 0.5rem auto;
 `;
 const Products = styled.div`
+  display: grid;
+  gap: 2.5rem;
+  grid-template-columns: 1fr;
+  grid-auto-rows: minmax(50px, auto);
+  width: 90vw;
+  max-width: 1240px;
+  margin: 4rem auto;
+  @media (min-width: 800px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+const Grid = styled.article`
+  display: block;
+`;
+
+const ProductInfo = styled.div`
   display: flex;
+  justify-content: space-between;
+`;
+
+const Img = styled.img`
+  max-width: 100vw;
 `;
--- src/context.js
+++ src/context.js
@@ -1,18 +1,44 @@
 import React, { useEffect, useContext, useState } from 'react';
+import axios from 'axios';
 
 const AppContext = React.createContext();
 
+const products_url = 'https://course-api.com/react-store-products';
+
 const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [products, setProducts] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
+
+  async function getFeatured() {
+    setLoading(true);
+    try {
+      const response = await axios.get(products_url);
+      const data = response.data;
+
+      setProducts(data);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getFeatured();
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
         handleClick,
         isSidebarOpen,
+        getFeatured,
+        products,
+        loading,
       }}
     >
       {children}
